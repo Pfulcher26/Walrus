270b96d7868b47fe697e0ae608004080434b2e9b
Payne Fulcher <pfulcher26@gmail.com>
2020-06-02 09:58:22 -0700

    add toggle function
    Co-authored by: Kelly Eidsvik <kelly.eidsvik@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,6 @@
 $(document).ready(function() {
   $("p").click(function() {
-    $(".definition-showing").toggle();
-  });
-
-$("p3").click(function() {
-    $(".definition-showing2").toggle();
+    $(".walrus-showing").toggle();
+    $(".walrus-hidden").toggle();
   });
 });
