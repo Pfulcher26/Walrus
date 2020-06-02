551fdeb18b7ee8f79c58a37d25fe81d9c5942b68
Payne Fulcher <pfulcher26@gmail.com>
2020-06-02 11:20:05 -0700

    Removes span classes, replaces with specific HTML elements in order to ensure functionality of toggle feature
    Co-authored by: Kelly Eidsvik <kelly.eidsvik@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,14 @@
 $(document).ready(function() {
-  $(".clickable").click(function() {
+ 
+  $("p").click(function() {
     $("#walrus-showing").slideToggle();
     $("#walrus-hidden").slideToggle();
   });
-});
+
+  $("p2").click(function() {
+    $("#walrus-showing2").slideToggle();
+    $("#walrus-hidden2").slideToggle();
+  });
+  
+  });
+
